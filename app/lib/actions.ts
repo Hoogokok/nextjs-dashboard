eb3e3c21ad86454bbcf41ade728341324c6de420
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-09-06 16:04:16 +0900

    feat: action.ts 파일에 로그인 인증 함수 추가
    - actions.ts 파일에 signIn 함수를 import하고, authenticate 함수를 추가했습니다.
    - authenticate 함수는 이전 상태와 폼 데이터를 받아서 credentials 방식으로 로그인을 시도합니다.
    - 로그인에 실패한 경우에는 AuthError를 처리하고, 유효하지 않은 자격 증명인 경우와 그 외의 오류에 대해 각각 다른 메시지를 반환합니다.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,13 +3,15 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+ 
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string({
         invalid_type_error: '고객을 선택해야 합니다.',
     }),
-    amount: z.coerce.number().gt(0, '금액은 0보다 커야 합니다.'),
+    amount: z.coerce.number().gt(0, { message: '금액은 0보다 커야 합니다.' }),
     status: z.enum(['pending', 'paid'], {
         invalid_type_error: '상태를 선택해야 합니다.',
     }),
@@ -39,8 +41,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
         return {
             errors: validatedFields.error.flatten().fieldErrors,
             message: '양식 오류: 인보이스를 만들 수 없습니다.',
-        }
+        };
     }
+
     const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
@@ -97,5 +100,25 @@ export async function deleteInvoice(id: string) {
             message: '데이터베이스 오류: 인보이스를 삭제할 수 없습니다.',
         }
     }
-    revalidatePath('/dashboard/invoices');
 }
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
+  
